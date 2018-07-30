8423406895c567d1bb5f955c03ff641a60249a18
codingkasia <codingkasia@gmail.com>
2018-07-30 16:34:59 -0400

    patching row with old lucky

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -19,18 +19,28 @@ class GuessesController < ApplicationController
       GuessesChannel.broadcast_to room, serialized_data
       head :ok
     end
+    
+  end
+
+  def update 
+    g = Guess.find(params[:id])
+    g.update(bgColor: "orange")
+  end
+
   end
   
   private
-  def point_params
-    params.require(:point).permit(:points, :user_id)
-  end
+      def point_params
+        params.require(:point).permit(:points, :user_id)
+      end
 
 
-  def guess_params
-    params.require(:guess).permit(:value, :room_id, :bgColor, :lucky, :user_id, :temp)
-  end
-end
+      def guess_params
+        params.require(:guess).permit(:value, :room_id, :bgColor, :lucky, :user_id, :temp)
+      end
+
+
+
 # def edit 
     #   guess = Guess.find(guess_params)
     # user_guess = UserGuess.find(guess: guess, user: User.find(params[:user_id][0]))
