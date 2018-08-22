ab5a5c0d5d06a2cbcd70ff1b4a856eab3c813c0a
codingkasia <codingkasia@gmail.com>
2018-08-22 18:02:42 -0400

    working basic version

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,3 @@
-# Rails.application.routes.draw do
-  
-#   #routes for login
-#   post 'login', to: 'auth#login'
-#   get '/user', to: 'users#show_user'
-
-  
-#   resources :users, only: [:index]
-#   resources :rooms, only: [:index, :create]
-  
-#   resources :guesses, only: [:create]
-#   resources :user_guesses, only: [:index]
-#   mount ActionCable.server => '/cable'
-
-# end
-
 Rails.application.routes.draw do
   
   #routes for login
